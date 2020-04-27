b71c2962c5d10dd8ecfaffc1a694a39eafb80042
David <davidcobbina47@gmail.com>
2020-04-27 12:04:39 +0000

    added injectable and linting

--- lib/application/auth/sign_in_form/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
+
 import 'package:bloc/bloc.dart';
-import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:meta/meta.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:dartz/dartz.dart';
 import 'package:googlekeepclone/domain/auth/auth_failure.dart';
-import 'package:googlekeepclone/domain/auth/i_auth_facade.dart';
+import 'package:flutter/foundation.dart';
 import 'package:googlekeepclone/domain/auth/value_objects.dart';
+import 'package:googlekeepclone/domain/auth/i_auth_facade.dart';
 
 part 'sign_in_form_event.dart';
 
@@ -12,6 +16,7 @@ part 'sign_in_form_state.dart';
 
 part 'sign_in_form_bloc.freezed.dart';
 
+@injectable
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   final IAuthFacade _authFacade;
 
--- lib/infrastructure/auth/firebase_auth_facade.dart
+++ lib/infrastructure/auth/firebase_auth_facade.dart
@@ -6,7 +6,10 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googlekeepclone/domain/auth/auth_failure.dart';
 import 'package:googlekeepclone/domain/auth/i_auth_facade.dart';
 import 'package:googlekeepclone/domain/auth/value_objects.dart';
+import 'package:injectable/injectable.dart';
 
+@lazySingleton
+@RegisterAs(IAuthFacade)
 class FirebaseAuthFacade implements IAuthFacade {
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
@@ -66,7 +69,7 @@ class FirebaseAuthFacade implements IAuthFacade {
     try {
       final googleUser = await _googleSignIn.signIn();
       if (googleUser == null) {
-        return left(AuthFailure.cancelledByUser());
+        return left(const AuthFailure.cancelledByUser());
       }
 
       final googleAuthentication = await googleUser.authentication;
